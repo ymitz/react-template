19de307ea9ca168eba83969e5bf9547e8c5626c0
ymitz <ymitz32@gmail.com>
2019-04-29 19:22:52 +0900

    clean up stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-let defaultTextColor = '#fff'
 let defaultStyle ={
-  color : defaultTextColor
+  color : '#fff'
 }
 
 function Aggregate() {
   return (
     <div style={{width:'40%',display:'inline-block'}}> 
-      <h2 style={{defaultTextColor}}>Number and Text</h2>
+      <h2 style={{defaultStyle}}>Number and Text</h2>
     </div>
   )
 }
@@ -25,12 +23,12 @@ function Filter(){
 
 function PlayList (){
   return (
-    <div style = {{...defaultStyle,width:'25%'}}>
+    <div style = {{...defaultStyle,width:'40%',display:'inline-block'}}>
     <img/>
     <h3>
       PlayListName
     </h3>
-    <ul><li>Song 1 </li> <li>Song 2 </li><li> Song3</li></ul>
+    <ul><li>Song 1</li><li>Song 2 </li><li> Song3</li></ul>
     </div>
   )
 }
@@ -42,10 +40,13 @@ function App() {
 
   return (
     <div className="App">
-      <h1> Title</h1>
+      <h1 style={{...defaultStyle,'font-size' : '54px'}}> Title</h1>
       <Aggregate/>
       <Aggregate/>
       <Filter/>
+      <PlayList/>
+      <PlayList/>
+      <PlayList/>
       <PlayList/>
     </div>
   );
