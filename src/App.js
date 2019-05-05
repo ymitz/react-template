60f53168d51f0b913f20eba2e9533f925af89b18
ymitz <ymitz32@gmail.com>
2019-05-05 14:02:07 +0900

    format

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
 import React from 'react';
 import './App.css';
 
-let defaultStyle ={
-  color : '#fff'
+let defaultStyle = {
+  color: '#fff'
 }
 
 function Aggregate() {
   return (
-    <div style={{width:'40%',display:'inline-block'}}> 
-      <h2 style={{defaultStyle}}>Number and Text</h2>
+    <div style={{ width: '40%', display: 'inline-block' }}>
+      <h2 style={{ defaultStyle }}>Number and Text</h2>
     </div>
   )
 }
-function Filter(){
-  return(
-    <div style={{defaultStyle}}>
-      <img/>
-      <input type = 'text'/>
+function Filter() {
+  return (
+    <div style={{ defaultStyle }}>
+      <img />
+      <input type='text' />
     </div>
   )
 }
 
-function PlayList (){
+function PlayList() {
   return (
-    <div style = {{...defaultStyle,width:'40%',display:'inline-block'}}>
-    <img/>
-    <h3>
-      PlayListName
-    </h3>
-    <ul><li>Song 1</li><li>Song 2 </li><li> Song3</li></ul>
+    <div style={{ ...defaultStyle, width: '40%', display: 'inline-block' }}>
+      <img />
+      <h3>
+        PlayListName </h3>
+      <ul><li>Song 1</li><li>Song 2 </li><li> Song3</li></ul>
     </div>
   )
 }
@@ -36,18 +35,18 @@ function PlayList (){
 function App() {
   let name = 'MTGME'
   let color = 'red'
-  let headerStyle ={color : color,'font-size' : '50px'}
+  let headerStyle = { color: color, 'font-size': '50px' }
 
   return (
     <div className="App">
-      <h1 style={{...defaultStyle,'font-size' : '54px'}}> Title</h1>
-      <Aggregate/>
-      <Aggregate/>
-      <Filter/>
-      <PlayList/>
-      <PlayList/>
-      <PlayList/>
-      <PlayList/>
+      <h1 style={{ ...defaultStyle, 'font-size': '54px' }}> Title</h1>
+      <Aggregate />
+      <Aggregate />
+      <Filter />
+      <PlayList />
+      <PlayList />
+      <PlayList />
+      <PlayList />
     </div>
   );
 }
