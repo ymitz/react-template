c8d7b8e63d379cb9ff4b6ed7b067e07ee5629bd3
ymitz <ymitz32@gmail.com>
2019-05-05 16:41:27 +0900

    chage react hook to class base

--- src/App.js
+++ src/App.js
@@ -1,54 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 let defaultStyle = {
   color: '#fff'
 }
-
-function Aggregate() {
-  return (
-    <div style={{ width: '40%', display: 'inline-block' }}>
-      <h2 style={{ defaultStyle }}>Number and Text</h2>
-    </div>
-  )
+let fakeServerData = {
+  user: {
+    name: "YMITSUGI"
+  }
 }
-function Filter() {
-  return (
+
+class Aggregate extends Component {
+  render() {
+    return (
+      <div style={{ width: '40%', display: 'inline-block' }}>
+        <h2 style={{ defaultStyle }}>Number and Text</h2>
+      </div>
+    )
+  }
+}
+class Filter extends Component {
+  render (){
+   return (
     <div style={{ defaultStyle }}>
       <img />
       <input type='text' />
     </div>
-  )
+  )   
+  }
 }
 
-function PlayList() {
-  return (
-    <div style={{ ...defaultStyle, width: '40%', display: 'inline-block' }}>
-      <img />
-      <h3>
-        PlayListName </h3>
-      <ul><li>Song 1</li><li>Song 2 </li><li> Song3</li></ul>
-    </div>
-  )
+class PlayList extends Component {
+  render() {
+    return (
+      <div style={{ ...defaultStyle, width: '40%', display: 'inline-block' }}>
+        <img />
+        <h3>
+          PlayListName </h3>
+        <ul><li>Song 1</li><li>Song 2 </li><li> Song3</li></ul>
+      </div>
+    )
+  }
+
 }
 
-function App() {
-  let name = 'MTGME'
-  let color = 'red'
-  let headerStyle = { color: color, 'font-size': '50px' }
-
-  return (
-    <div className="App">
-      <h1 style={{ ...defaultStyle, 'font-size': '54px' }}> Title</h1>
-      <Aggregate />
-      <Aggregate />
-      <Filter />
-      <PlayList />
-      <PlayList />
-      <PlayList />
-      <PlayList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    let name = 'MTGME'
+    let color = 'red'
+    let headerStyle = { color: color, 'font-size': '50px' }
+    return (
+      <div className="App">
+        <h1 style={{ ...defaultStyle, 'font-size': '54px' }}>
+          {fakeServerData.user.name}
+        </h1>
+        <Aggregate />
+        <Aggregate />
+        <Filter />
+        <PlayList />
+        <PlayList />
+        <PlayList />
+        <PlayList />
+      </div>
+    );
+  }
 }
 
 export default App;
