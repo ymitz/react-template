65df4ae4bf6908f4cc306c583486b3e6f02fb5ed
ymitz <ymitz32@gmail.com>
2019-05-12 19:18:59 +0900

    change login callback

--- src/App.js
+++ src/App.js
@@ -129,6 +129,8 @@ class App extends Component {
     let parsed = queryString.parse(window.location.search)
     let accessToken = parsed.access_token
 
+    if(!accessToken) return
+
     fetch('https://api.spotify.com/v1/me', {
       headers: { 'Authorization': 'Bearer ' + accessToken }
     }).then(response => response.json())
@@ -194,7 +196,11 @@ class App extends Component {
                 }
               </div>
             }
-          </div> : <button onClick={() => window.location = 'http://localhost:8888/login'}
+          </div> : <button onClick={() => {
+            window.location = window.location.href.includes('localhost') ?
+            'http://localhost:8888/login'
+            :'https://tfb-backend.herokuapp.com/login'
+          }}
             style={{ padding: '20px', 'font-size': '50px', 'margin-top': '20px' }}>
             Sign in with Spotify
           </button>
